5fe6d2d04fddd127fb19316da84eed5c2e1420a7
Kgaogel09 <kgaogelo@swipeix.com>
2022-03-17 10:19:38 +0200

    use state hook

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,9 +1,13 @@
+import FeedbackItem from "./components/FeedbackItem"
 import Header from "./components/Header"
 
 function App() {
   return (
     <>
       <Header />
+      <div className='container'>
+        <FeedbackItem />
+      </div>
     </>
   )
 }
