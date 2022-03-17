3adee5c2e661613e68bf09d19023ad70088162a7
Kgaogel09 <kgaogelo@swipeix.com>
2022-03-17 10:45:04 +0200

    global state

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,12 +1,15 @@
-import FeedbackItem from "./components/FeedbackItem"
+import { useState } from "react"
+import FeedbackList from "./components/FeedbackList"
 import Header from "./components/Header"
+import FeedBackData from "./data/FeedbackData"
 
 function App() {
+  const [feedback, setFeedBack] = useState(FeedBackData)
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackItem />
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   )
