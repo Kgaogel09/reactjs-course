09e10b86903e41967199e5d8cb3f6a5a7ae1dc5e
Kgaogel09 <kgaogelo@swipeix.com>
2022-03-17 09:54:47 +0200

    adding styles

--- feedback-app/src/components/Header.jsx
+++ feedback-app/src/components/Header.jsx
@@ -1,8 +1,13 @@
 import PropTypes from "prop-types"
 
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  }
+
   return (
-    <header>
+    <header style={headerStyles}>
       <div className='container'>
         <h2>{text}</h2>
       </div>
@@ -11,11 +16,15 @@ function Header({ text }) {
 }
 
 Header.defaultProps = {
-  text: "I love My Life",
+  text: "Feedback UI",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
 }
 
 Header.propTypes = {
   text: PropTypes.string.isRequired,
+  bgColor: PropTypes.string.isRequired,
+  textColor: PropTypes.string.isRequired,
 }
 
 export default Header
