3e25eadc55f239b4a1bc02134d032090c26831aa
Kgaogel09 <kgaogelo@swipeix.com>
2022-03-27 00:25:03 +0200

    APIs explained

--- feedback-app/src/data/FeedbackData.js
+++ feedback-app/src/data/FeedbackData.js
@@ -17,3 +17,18 @@ const FeedBackData = [
 ]
 
 export default FeedBackData
+
+// Data is kept in a database on a server
+// There is some kind of backend API
+// The API has routes that we can access from the client(React) in order to create, read, update and delete data
+// Data is brought into our state and used in our react app
+// REST API - Representational State Transfer
+// GET - Retrieves data from the server
+// POST - Submits data to the server
+// PUT/PATCH - updates data on the server
+// DELETE - Deletes data on the server
+// 1XX:Informational - Request received
+// 2XX:Success - Successfully received
+// 3XX:Redirect - further action must be taken/redirect
+// 4XX:Client Error - Request does not have what it needs
+// 5XX:Server Error - Server failed to fulfill a valid request
